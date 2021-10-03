6dccd4a697c1f810fa7e37a1ef181054927d5305
YellowFoxH4XOR <yellowfoxh4xor@gmail.com>
2021-10-03 18:37:29 +0530

    Added initial api code

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/user/{username}")
+def read_user_details(username: str):
+    print(f"https://api.github.com/users/{username}")
+    resp = requests.get(f"https://api.github.com/users/{username}")
+    return {"result": resp.json()}
